073d1cb71fdd955dc828d379e95146382ce50886
Ni, Haowei [GTSUS] <HNi1@its.jnj.com>
2022-02-09 21:18:00 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the repo for Kaggle H&M recommendation competition',
+    author='Haowei Ni & Jixi Dai',
+    license='MIT',
+)
